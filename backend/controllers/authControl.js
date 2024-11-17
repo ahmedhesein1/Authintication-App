acc0ad3f0912112668a4641d555c7ee0a771f216
ahmedhesein1 <ahmedhesein1790@gmail.com>
2024-11-17 21:54:13 +0200

    Added Send Verification Email Function after SignUp

--- backend/controllers/authControl.js
+++ backend/controllers/authControl.js
@@ -5,21 +5,18 @@ import User from "../models/userModel.js";
 import AppError from "../utils/AppError.js";
 import jwt from "jsonwebtoken";
 import { generateVerificationCode } from "../utils/generateVerificationCode.js";
-
+import {sendVerificationEmail} from "../mailtrap/emails.js";
 export const signup = asyncHandler(async (req, res, next) => {
   const { name, email, password } = req.body;
 
-  // Check if user already exists
   const user = await User.findOne({ email });
   if (user) {
     return next(new AppError("User already exists", 400));
   }
 
-  // Hash password and generate verification token
   const hashedPassword = await bcrypt.hash(password, 10);
   const verificationToken = generateVerificationCode();
 
-  // Create new user
   const newUser = await User.create({
     email,
     password: hashedPassword,
@@ -27,15 +24,13 @@ export const signup = asyncHandler(async (req, res, next) => {
     verificationToken,
     verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hours from now
   });
-
+  await sendVerificationEmail(email, verificationToken);
   res.status(201).json({
     success: true,
     message: "User created successfully",
     user: {
-      email: newUser.email,
-      name: newUser.name,
-      verificationToken: newUser.verificationToken,
-      verificationTokenExpiresAt: newUser.verificationTokenExpiresAt,
+      ...newUser._doc,
+      password: undefined,
     },
   });
 });
