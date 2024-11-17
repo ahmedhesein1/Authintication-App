5c6a86ece5e67de7e0ba40082f88e939b154cb08
ahmedhesein1 <ahmedhesein1790@gmail.com>
2024-11-17 20:54:29 +0200

    Added globalErrorHandler Function

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
+import globalErrorHandler from "./controllers/errorControl.js";
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.DB)
@@ -14,7 +18,8 @@ mongoose
   .catch((err) => {
     console.log("Database connection Field ");
   });
-
+app.use('/api/auth', authRoutes)
+app.use(globalErrorHandler)
 const port = 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
