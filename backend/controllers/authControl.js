e9e07d9408ef2c01ebbda5197721a776d2a56983
ahmedhesein1 <ahmedhesein1790@gmail.com>
2024-11-18 21:08:23 +0200

    Implementing Login , verify Password and Logout Functions

--- backend/controllers/authControl.js
+++ backend/controllers/authControl.js
@@ -5,7 +5,7 @@ import User from "../models/userModel.js";
 import AppError from "../utils/AppError.js";
 import jwt from "jsonwebtoken";
 import { generateVerificationCode } from "../utils/generateVerificationCode.js";
-import {sendVerificationEmail} from "../mailtrap/emails.js";
+import { sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js";
 export const signup = asyncHandler(async (req, res, next) => {
   const { name, email, password } = req.body;
 
@@ -33,4 +33,69 @@ export const signup = asyncHandler(async (req, res, next) => {
       password: undefined,
     },
   });
+});
+export const verifyEmail = asyncHandler(async (req, res, next) => {
+  const { code } = req.body;
+  const user = await User.findOne({
+    verificationToken: code,
+    verificationTokenExpiresAt: { $gt: Date.now() },
+  });
+  if (!user) {
+    return next(new AppError("Invalid or Expired code", 404));
+  }
+  user.isVerified = true;
+  user.verificationToken = undefined;
+  user.verificationTokenExpiresAt = undefined;
+  await user.save();
+  await sendWelcomeEmail(user.email, user.name);
+  res.status(200).json({
+    success: true,
+    message: "Email verified successfully",
+    user: {
+      ...user._doc,
+      password: undefined,
+    },
+  });
+  next();
+});
+export const login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  const isMatched = await bcrypt.compare(password, user.password);
+  if (!user || !isMatched) {
+    return next(new AppError("Email or Password Is Not Correct", 400));
+  }
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "90d ",
+  });
+  user.lastLogin = new Date();
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+    })
+    .status(200)
+    .json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+        token,
+      },
+    });
+});
+export const logout = asyncHandler(async (req, res, next) => {
+  res.clearCookie("token");
+  res.status(200).json({
+    success: true,
+    message: "Logged out successfully",
+  });
+});
+export const forgotPassword = asyncHandler(async (req, res, next) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(new AppError("User not found", 400));
+  }
+  
 });
