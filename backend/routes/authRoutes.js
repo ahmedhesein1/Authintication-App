dc196d199dc2b147244885c69954df1eb9d375ca
ahmedhesein1 <ahmedhesein1790@gmail.com>
2024-11-18 22:25:35 +0200

    Reset Password Routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,15 +4,15 @@ import * as authControl from "../controllers/authControl.js";
 
 const router = express.Router();
 
-// router.get("/check-auth", verifyToken, checkAuth);
+router.get("/check-auth", authControl.protect, authControl.checkAuth);
 
 router.post("/signup", authControl.signup);
 router.post("/login", authControl.login);
-// router.post("/logout", logout);
+router.post("/logout", authControl.logout);
 
 router.post("/verify-email", authControl.verifyEmail);
-// router.post("/forgot-password", forgotPassword);
+router.post("/forgot-password", authControl.forgotPassword);
 
-// router.post("/reset-password/:token", resetPassword);
+router.post("/reset-password/:token", authControl.resetPassword);
 
 export default router;
